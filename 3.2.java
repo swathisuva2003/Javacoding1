5e59d888c59f0acc74bd99b7c4ce6d1c791a858d
swathisuva2003 <159701434+swathisuva2003@users.noreply.github.com>
2024-07-14 21:10:02 +0530

    3.2.java

---
+++ 3.2.java
@@ -0,0 +1,10 @@
+
+//3.Print alternate numbers starting from 0 to 100
+
+public class PrintEvenNumbers {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i += 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
