8ab8227084357030c7d18cc1c6516feb29697496
swathisuva2003 <159701434+swathisuva2003@users.noreply.github.com>
2024-07-15 09:16:27 +0530

    Update 1.1.java

--- 1.1.java
+++ 1.1.java
@@ -2,13 +2,10 @@
 
 public class AlphabetPattern {
     public static void main(String[] args) {
-        // Loop for rows (from A to E)
         for (char ch = 'A'; ch <= 'E'; ch++) {
-            // Loop for printing each character multiple times
             for (int j = 0; j < (ch - 'A' + 1); j++) {
                 System.out.print(ch);
             }
-            // Move to the next line after each row
             System.out.println();
         }
     }
