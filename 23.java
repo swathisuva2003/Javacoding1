530e544e047328767ace2c77d489f169e82ad56f
swathisuva2003 <159701434+swathisuva2003@users.noreply.github.com>
2024-07-15 11:34:48 +0530

    Create 23.java

---
+++ 23.java
@@ -0,0 +1,28 @@
+23)//Wrapper Class 
+public class Wrapper
+{    
+    public static void main(String args[])
+       {    
+           float f=50.0F;  
+           Float floatobj=f;  
+           int i=3;
+           Integer intobj=i;
+           byte b=6;
+           Byte byteobj=b;
+           char c='c';
+           Character charobj=c;
+           long l=80;
+           Long longobj=l;
+           double d=1534.2;
+           Double doubleobj=d;
+           boolean b2=true;
+           Boolean boolobj=b2;
+           System.out.println(floatobj);  
+           System.out.println(intobj);
+           System.out.println(byteobj);
+           System.out.println(charobj);
+           System.out.println(longobj);
+           System.out.println(doubleobj);
+           System.out.println(boolobj);
+       }
+}
